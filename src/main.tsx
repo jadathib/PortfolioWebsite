bcaf50781b349eb3f7cc0893feb73d01b55c2a55
Jada <jada.r.thibodeaux@gmail.com>
2025-09-07 07:14:13 -0500

    revert changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
-import { BrowserRouter } from "react-router-dom";
-import * as React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import * as React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import './index.css';
 
-createRoot(document.getElementById("root")!).render(
-    <React.StrictMode>
-        <BrowserRouter>
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+    createRoot(rootElement).render(
+        <React.StrictMode>
             <App />
-        </BrowserRouter>
-    </React.StrictMode>
-);
+        </React.StrictMode>
+    );
+}
